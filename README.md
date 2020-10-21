15f1962702c7c60ce1f71f35a001a0265dabe879
omrilotan <omri@hamadgera.com>
2020-10-21 21:52:43 +0300

    💬 Update current STDOUT stream

---
+++ README.md
@@ -0,0 +1,28 @@
+# stdline [![](https://img.shields.io/npm/v/stdline.svg)](https://www.npmjs.com/package/stdline) [![](https://img.shields.io/badge/source--000000.svg?logo=github&style=social)](https://github.com/omrilotan/stdline)
+
+## 💬 Update current STDOUT stream
+
+![](https://user-images.githubusercontent.com/516342/96769099-576bfc80-13e7-11eb-92f6-75988a5bbde2.gif)
+
+
+Work on current line
+```js
+const {
+	update,
+	end,
+} = require('stdline');
+
+console.log('processing:');
+...
+
+update('Got an update for you');
+...
+end('Finished!');
+```
+
+Clear screen
+```js
+const { clear } = require('stdline');
+
+clear();
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+const {
+	clearLine,
+	clearScreenDown,
+	cursorTo,
+} = require('readline');
+
+const { stdout } = process;
+
+/**
+ * Update current STDOUT line
+ * @param  {String} message
+ * no return value
+ */
+exports.update = function update(message) {
+	clearLine(stdout, 0); // Clear current STDOUT line
+	cursorTo(stdout, 0); // Place cursor at the start
+	stdout.write(message.toString());
+}
+
+/**
+ * Update current STDOUT line and move to next one
+ * @param  {String} message
+ * no return value
+ */
+exports.end = function end(message = '') {
+	exports.update([ message.toString(), '\n' ].join(''));
+}
+
+/**
+ * Clear current STDOUT stream
+ * no return value
+ */
+exports.clear = function clear() {
+	cursorTo(stdout, 0, 0);
+	clearScreenDown(stdout);
+}
